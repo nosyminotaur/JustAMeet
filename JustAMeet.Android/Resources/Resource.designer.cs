d92d40d609b25b92f905ef2c704ab81378c2be1e
Brinden24561 <39349733+Brinden24561@users.noreply.github.com>
2019-07-04 19:41:55 -0500

    Worked on the backrounds and messages boxes. But kept getting an error "Data at the root level is invalid Line1, position 1."

--- JustAMeet.Android/Resources/Resource.designer.cs
+++ JustAMeet.Android/Resources/Resource.designer.cs
@@ -5778,26 +5778,26 @@ namespace JustAMeet.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
+			// aapt resource value: 0x7f02013b
+			public const int avd_hide_password_1 = 2130837819;
+			
+			// aapt resource value: 0x7f02013c
+			public const int avd_hide_password_2 = 2130837820;
+			
 			// aapt resource value: 0x7f02013d
-			public const int avd_hide_password_1 = 2130837821;
-			
-			// aapt resource value: 0x7f02013e
-			public const int avd_hide_password_2 = 2130837822;
-			
-			// aapt resource value: 0x7f02013f
-			public const int avd_hide_password_3 = 2130837823;
+			public const int avd_hide_password_3 = 2130837821;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
+			// aapt resource value: 0x7f02013e
+			public const int avd_show_password_1 = 2130837822;
+			
+			// aapt resource value: 0x7f02013f
+			public const int avd_show_password_2 = 2130837823;
+			
 			// aapt resource value: 0x7f020140
-			public const int avd_show_password_1 = 2130837824;
-			
-			// aapt resource value: 0x7f020141
-			public const int avd_show_password_2 = 2130837825;
-			
-			// aapt resource value: 0x7f020142
-			public const int avd_show_password_3 = 2130837826;
+			public const int avd_show_password_3 = 2130837824;
 			
 			// aapt resource value: 0x7f020056
 			public const int design_bottom_navigation_item_background = 2130837590;
@@ -6462,11 +6462,11 @@ namespace JustAMeet.Droid
 			// aapt resource value: 0x7f020132
 			public const int notification_icon_background = 2130837810;
 			
-			// aapt resource value: 0x7f02013b
-			public const int notification_template_icon_bg = 2130837819;
+			// aapt resource value: 0x7f020139
+			public const int notification_template_icon_bg = 2130837817;
 			
-			// aapt resource value: 0x7f02013c
-			public const int notification_template_icon_low_bg = 2130837820;
+			// aapt resource value: 0x7f02013a
+			public const int notification_template_icon_low_bg = 2130837818;
 			
 			// aapt resource value: 0x7f020133
 			public const int notification_tile_bg = 2130837811;
@@ -6478,19 +6478,13 @@ namespace JustAMeet.Droid
 			public const int signin_button = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int signin_button_pressed = 2130837814;
+			public const int signup_button = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int signup_button = 2130837815;
+			public const int tooltip_frame_dark = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int signup_button_pressed = 2130837816;
-			
-			// aapt resource value: 0x7f020139
-			public const int tooltip_frame_dark = 2130837817;
-			
-			// aapt resource value: 0x7f02013a
-			public const int tooltip_frame_light = 2130837818;
+			public const int tooltip_frame_light = 2130837816;
 			
 			static Drawable()
 			{
