08f35551e14625e39e41f976e034163ed58659eb
Brinden24561 <39349733+Brinden24561@users.noreply.github.com>
2019-07-10 21:46:56 -0500

    Added arrow on Sign-up and Started working on the AboutMe Page change. Also adjusted the loginpage slightly to fit my android screen. If it doesn't work for yours feel free to change back.

---
+++ JustAMeet/Views/AboutME.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JustAMeet.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutMe : ContentPage
+    {
+        public AboutMe()
+        {
+            InitializeComponent();
+        }
+    }
+}
